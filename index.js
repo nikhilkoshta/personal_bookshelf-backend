a4e043d070bb4cbd7c918c84844c786ef5a06014
Nikhil <72139039+nikhilkoshta@users.noreply.github.com>
2024-06-04 20:27:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://personal-bookshelf-backend.vercel.app/books',
+  origin: 'https://personal-bookshelf-frontend.vercel.app/books',
   methods: ['GET', 'POST'],
   headers: ['Authorization'],
   credentials: true,
